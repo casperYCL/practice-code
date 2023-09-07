85fa1480442e536dc4801527eb69d01a84fa8c7f
Casper <casper@gmail.com>
2023-09-07 22:34:40 +0800

    '2done'

---
+++ 20211117.py
@@ -0,0 +1,42 @@
+# a=[1,2]
+# b=[3,4]
+# c=a+b
+# a.extend(b)
+# print(c)
+# print(a)
+
+
+# a='deep learning'
+# a=list(a)
+# print(a)
+# print(a[3])
+
+
+# a = [23,65,98,325,65]
+# x1,x2,x3,x4,x5 = a
+# print(x1,x2,x5)
+
+
+# i=0
+# sum=0
+# list1=[1,2,3,4,5,6,7,8,9,10]
+# for i in list1:
+#     sum=sum+i
+# print(sum)
+
+
+# a=['Mike','John','Marry']
+# b=a[0]
+# c=a[1]
+# d=a[2]
+# b=list(b)
+# c=list(c)
+# d=list(d)
+# print(b[0])
+# print(c[0])
+# print(d[0])
+
+
+
+
+
