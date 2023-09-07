86ed86c15e60d19a40bf76dcb4a387532d62d499
Casper <casper@gmail.com>
2023-09-07 22:55:51 +0800

    '2done'

---
+++ bankkkk.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 21 14:43:33 2021
+
+@author: user
+"""
+from bank import Banks
+
+aaabank=Banks('aaabank',2000)
+# print(aaabank.name.title())
+print(aaabank.get_balance())
