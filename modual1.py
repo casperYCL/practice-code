ac119c7e503773de71b70f87311e71de6c65688a
Casper <casper@gmail.com>
2023-09-07 23:01:11 +0800

    '2done'

---
+++ modual1.py
@@ -0,0 +1,31 @@
+# import modual
+# a=modual.Cars()
+# print(a.drive('blue',4))
+
+
+
+
+
+import modu
+   
+# Created a class object
+object = module.GFG()
+   
+# Calling and printing class methods
+print(object.add(15,5))
+print(object.sub(15,5))
+   
+# Calling the function
+module.method()
+
+
+
+
+
+
+
+
+
+
+
+
